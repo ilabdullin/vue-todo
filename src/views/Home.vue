0dfcfffa688d5c167df34b0d316b4d3185972316
ilabdullin <ilnurabdullin578@gmail.com>
2021-10-21 17:27:12 +0500

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h2>Home page</h2>
+
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita,
+            dolor unde! Iste autem unde, fugiat hic repudiandae, quam animi quae
+            pariatur non suscipit dicta quod! Possimus expedita atque modi
+            voluptatibus? Aliquid iusto dolorem alias iure maxime blanditiis in?
+            Sunt quo beatae, deleniti molestias in itaque dolore voluptates vero
+            a unde ad enim. Voluptatum, saepe suscipit possimus nisi quaerat hic
+            ducimus?
+        </p>
+
+        <router-link to="/todos">Todos</router-link>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
